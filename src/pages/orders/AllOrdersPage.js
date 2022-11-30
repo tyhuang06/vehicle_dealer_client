ddad5d66b31d422cdead6b6f4e052a124dfe8448
Jamie Huang <tyhuang06@gmail.com>
2022-11-30 13:10:16 -0500

    feat(orders): created all orders layout

--- src/pages/orders/AllOrdersPage.js
+++ src/pages/orders/AllOrdersPage.js
@@ -1,4 +1,17 @@
 import { React, useEffect, useState } from 'react';
+import {
+	Container,
+	Card,
+	Heading,
+	CardHeader,
+	CardBody,
+	Stack,
+	StackDivider,
+	Box,
+	Text,
+	Button,
+} from '@chakra-ui/react';
+import dateFormat from 'dateformat';
 import OrderService from '../../services/OrderService';
 
 const AllOrdersPage = () => {
@@ -15,11 +28,44 @@ const AllOrdersPage = () => {
 	}, []);
 
 	return (
-		<div>
-			{orders.map((order) => (
-				<div>{order.order_id}</div>
-			))}
-		</div>
+		<Container className="pt-8">
+			<Card>
+				<CardHeader>
+					<Heading size="md">All Orders</Heading>
+				</CardHeader>
+
+				<CardBody>
+					<Stack divider={<StackDivider />} spacing="4">
+						{orders.map((order) => (
+							<Box className="flex justify-between">
+								<div>
+									<Heading
+										size="xs"
+										textTransform="uppercase"
+									>
+										Order: {order.order_id}
+									</Heading>
+									<Text pt="2" fontSize="sm">
+										{dateFormat(
+											order.order_date,
+											'mm/dd/yyyy'
+										)}
+									</Text>
+								</div>
+								<div className="flex items-center">
+									<Button
+										colorScheme="teal"
+										variant="outline"
+									>
+										Details
+									</Button>
+								</div>
+							</Box>
+						))}
+					</Stack>
+				</CardBody>
+			</Card>
+		</Container>
 	);
 };
 

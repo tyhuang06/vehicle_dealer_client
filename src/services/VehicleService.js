4e7cabce13ff18674287cf2f3a2d8b6631acc693
kaiqiz8 <kzhang49@binghamton.edu>
2022-12-02 19:54:29 -0500

    update VehicleService.js

--- src/services/VehicleService.js
+++ src/services/VehicleService.js
@@ -1,14 +1,69 @@
 import { axiosDefault } from '../axios';
 
+const getAllVehicles = async() => {
+	const {data} = await axiosDefault.get('/vehicle');
+	return data;
+}
+
 const getVehicleById = async (id) => {
 	const { data } = await axiosDefault.get(`/vehicle/${id}`);
 	return data;
 };
 
+const getVehicleByType = async (type) => {
+	const { data } = await axiosDefault.get(`/vehicle/${type}`);
+	return data;
+}
+
+const getVehicleByBrand = async (brand) => {
+	const { data } = await axiosDefault.get(`/vehicle/${brand}`);
+	return data;
+}
+
+const getElectricVehicle = async () => {
+	const {data} = await axiosDefault.get('/vehicle/electric');
+	return data;
+}
+
+const getNewVehicle = async() => {
+	const {data} = await axiosDefault.get('/vehicle/newVehicle');
+	return data;
+}
+
+const getUsedVehicle = async() => {
+	const {data} = await axiosDefault.get('/vehicle/usedVehicle');
+	return data;
+}
+
+const getVehicleAfterYear = async() => {
+	const {data} = await axiosDefault.get('/vehicle/year');
+	return data;
+}
+
+const getVehicleByColor = async() => {
+	const {data} = await axiosDefault.get('/vehicle/color');
+	return data;
+}
+
+const getVehicleByPrice = async() => {
+	const {data} = await axiosDefault.get('/vehicle/price');
+	return data;
+}
+
+
 // TODO: Add the rest of the vehicle operations
 
 const VehicleService = {
+	getAllVehicles,
+	getVehicleByType,
+	getVehicleByBrand,
 	getVehicleById,
+	getElectricVehicle,
+	getNewVehicle,
+	getUsedVehicle,
+	getVehicleAfterYear,
+	getVehicleByColor,
+	getVehicleByPrice,
 };
 
 export default VehicleService;
